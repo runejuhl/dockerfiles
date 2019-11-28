5d2ae031400704be57f3035ec8b8a559782525af
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2019-11-28 09:48:43 +0100

    Update graalvm and lein-graal images

--- graalvm/metadata.sh
+++ graalvm/metadata.sh
@@ -1,2 +1,3 @@
-GRAALVM_VERSION=19.0.2
+GRAALVM_VERSION=19.2.1
 VERSION=$GRAALVM_VERSION
+DOWNLOAD_URL="https://github.com/oracle/graal/releases/download/vm-${GRAALVM_VERSION}/graalvm-ce-linux-amd64-${GRAALVM_VERSION}.tar.gz"
