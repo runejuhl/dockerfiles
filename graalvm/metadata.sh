beaee3134bba7b9a87cee9819dcff25ba78249e4
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2021-06-12 17:18:01 +0200

    Update graalvm and lein-graal images to use Graal 21.1.0

--- graalvm/metadata.sh
+++ graalvm/metadata.sh
@@ -1,4 +1,4 @@
-GRAALVM_VERSION=20.3.0
+GRAALVM_VERSION=21.1.0
 GRAALVM_RELEASE=ce-java11
 VERSION=$GRAALVM_VERSION
 DOWNLOAD_URL="https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${GRAALVM_VERSION}/graalvm-${GRAALVM_RELEASE}-linux-amd64-${GRAALVM_VERSION}.tar.gz"
